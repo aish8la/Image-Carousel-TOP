bea9d8ee649d721fdbdd19fbcc8a23163f0d3e33
aish8la <64914670+aish8la@users.noreply.github.com.>
2025-01-25 05:33:05 +0500

    Implement image carousel navigation functionality and update styles

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import "./styles.css";
 
+const nextBtn = document.querySelector(".next-img-btn");
+const previous = document.querySelector(".previous-btn");
+const imageCtn = document.querySelector(".image-ctn");
+let rightValue = 0;
+
+function nextClick() {
+    rightValue += ((1 / 6) * 100);
+    imageCtn.setAttribute("style", `transform: translate(-${rightValue}%, 0%)`);
+}
+
+nextBtn.addEventListener("click", nextClick);
